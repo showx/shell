12cbc08630b768828e1f9e394bb0b6008828b61e
show <showshou@126.com>
2015-09-10 08:39:11 +0800

    day

---
+++ filestat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -ne 1];
+then
+	echo "Usage is $0 basepath";
+	exit
+fi
+path=$1;
+
+declare -A statarray;
+
+while read line;
+do 
+	ftype=`file -b "$line" | cut -d, -f1`
+	let statarray["$ftype"]++;
+
+done < <(find $path -type f -print)
+
+echo ===== file types and counts =====
+for ftype in "${!statarray[@]}";
+do 
+	echo $ftype : ${statarray["$ftype"]}
+done
